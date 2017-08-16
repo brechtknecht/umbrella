4df95868248dbe24cb00b459562d6d0fa094f79b
Fiete <felix.tesche@googlemail.com>
2017-08-16 23:21:55 +0200

    Added some Sunrise and Sunset Effects

--- www/scripts/colorController.js
+++ www/scripts/colorController.js
@@ -2,8 +2,8 @@ function colorHandler(colorData) {
     $.getJSON("./server/database/colorDatabase.json", function (data) {
         var colorData = data;
 
-        var currentHour = new Date().getHours();
-        //var currentHour = 24;
+        //var currentHour = new Date().getHours();
+        var currentHour = 11;
         
         var nextHour;
         var previousHour;
@@ -13,11 +13,19 @@ function colorHandler(colorData) {
         } else {
             nextHour = currentHour + (6 - (currentHour % 6));
         }
-
+        
         previousHour = nextHour - 6;
         if (previousHour < 0) {
             previousHour = 24;
         }
+        if(nextHour == 6){
+            $('.st6').toggleClass('sunrise');
+        }
+        if(nextHour == 24){
+            $('.st6').toggleClass('sunset');
+        }
+        
+        
 
         COLOR_TEMPLATE_PUSH(colorData[nextHour], colorData[previousHour]);
     });
@@ -28,8 +36,11 @@ function COLOR_TEMPLATE_PUSH(nextColorInformation, previousColorInformation) {
     console.log(previousColorInformation);
     var animationDuration = 7;
 
-    $('.phone-wrapper, .suggestion').css('background', previousColorInformation.Mountains.lowMountains);
+    $('.phone-wrapper, .suggestion').css('background', previousColorInformation.Mountains.lowMountains + '!important');
 
+    
+    console.log(previousColorInformation.Decoration.decorationBackground);
+    $('.st6').css({ fill:  previousColorInformation.Decoration.decorationBackground });
     
     $('#backgroundLow').html(' <animate  attributeName="stop-color" from="' + previousColorInformation.Background.lowBackground + '" to="' + nextColorInformation.Background.lowBackground + '" dur="' + animationDuration + '" fill="freeze"/>');
 
@@ -44,6 +55,8 @@ function COLOR_TEMPLATE_PUSH(nextColorInformation, previousColorInformation) {
     $('#highlightsHigh1, #highlightsHigh2, #highlightsHigh3, #highlightsHigh4').html(' <animate  attributeName="stop-color" from="' + previousColorInformation.MountainDetails.highMountainDetails + '" to="' + nextColorInformation.MountainDetails.highMountainDetails + '" dur="' + animationDuration + '" fill="freeze"/>');
 
     $('#highlightsLow1, #highlightsLow2, #highlightsLow3, #highlightsLow4').html(' <animate  attributeName="stop-color" from="' + previousColorInformation.MountainDetails.lowMountainDetails + '" to="' + nextColorInformation.MountainDetails.lowMountainDetails + '" dur="' + animationDuration + '" fill="freeze"/>');
+    
+    
 
     $('.phone-wrapper, .suggestion').css('background', nextColorInformation.MountainDetails.lowMountainDetails).css('transition:' + animationDuration  + 's ease-in');
 
--- www/scripts/script.js
+++ www/scripts/script.js
@@ -16,5 +16,7 @@ $(document).ready(function () {
     setTimeout(function () {
         determineClothesFromWeatherData();
     }, 2000);
+    
+    
 
 });
